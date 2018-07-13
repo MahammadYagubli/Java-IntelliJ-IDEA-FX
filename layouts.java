510f8039eef6700270f34cede11e962b8bdc40dc
Mhammad Yagubli <mahammad.yagubli@gmail.com>
2018-07-14 00:02:10 +0200

    Create layouts.java

---
+++ layouts.java
@@ -0,0 +1,20 @@
+<?import javafx.geometry.Insets?>
+<?import javafx.scene.layout.GridPane?>
+
+<?import javafx.scene.control.Button?>
+<?import javafx.scene.control.Label?>
+<?import javafx.scene.layout.ColumnConstraints?>
+<?import javafx.scene.layout.HBox?>
+<HBox fx:controller="sample.Controller" alignment="BOTTOM_RIGHT"
+             xmlns:fx="http://javafx.com/fxml"
+      style="-fx-border-color:red; -fx-width:3; -fx-border-style:dashed"
+spacing="10">
+    <padding>
+    <Insets bottom="10" right="10"/>
+
+    </padding>    <Button text="Button1"  />
+    <Button text="Button2" prefWidth="30" />
+    <Button text="Button3" prefWidth="30"  />
+    <Button text="Button4" prefWidth="30" />
+    <Button text="Button5" prefWidth="30"  />
+</HBox
